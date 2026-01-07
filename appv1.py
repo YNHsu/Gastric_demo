d3a47e2ae11b12ce89aee0800ab5128cd108e8bf
YNHsu <84616635+YNHsu@users.noreply.github.com>
2026-01-07 16:19:00 +0800

    Update appv1.py

--- appv1.py
+++ appv1.py
@@ -212,7 +212,7 @@ if imgname is not None:
     # ===== Step 5 =====
     st.subheader("Step 7: Gastric cancer prediction")
     if st.session_state.step == 5:
-        st.session_state.df = pd.DataFrame({'Feature': ['HP', 'AG_Antrum', 'AG_Body', 'IM_Antrum', 'IM_Body', 'Age', 'Gender'], 'P249750000282': [1, 0, 1, 1, 1, 66, 1]}) 
+        st.session_state.df = pd.DataFrame({'Feature': ['HP', 'AG_Antrum', 'AG_Body', 'IM_Antrum', 'IM_Body', 'Age', 'Sex'], 'P249750000282': [1, 0, 1, 1, 1, 66, 1]}) 
     time.sleep(3)
     if st.session_state.df is not None:
         st.write('Overall summary')
