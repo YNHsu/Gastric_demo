51ea48e6681765c732e2d832dc1b36ceda9fc7cb
YNHsu <84616635+YNHsu@users.noreply.github.com>
2026-04-27 16:08:24 +0800

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -22,6 +22,10 @@ st.markdown(unsafe_allow_html=True, body="<p>In this web, you can select the pat
 imgname = st.selectbox('Select the patient', (None, '00000', '00001'))
 # imgname = st.selectbox('Select the CT image', (None, 'CTbrain_1.png','CTbrain_2.png','CTbrain_3.png','CTbrain_4.png','CTbrain_5.png','CTbrain_6.png','CTbrain_7.png','CTbrain_8.png'))
 
+if st.button("Reset"):
+    st.session_state.clear()
+    st.rerun()
+
 if imgname == '00001':
     images = os.listdir('gastric_image/')
     st.session_state.images_count = len(images)
